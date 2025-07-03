9aec7ec3ebc64c52336d575bc3dbbf5d184159f8
HocvienSorian <hocviensorian@gmail.com>
2025-07-03 15:31:52 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,13 +14,15 @@ export default async function handler(req, res) {
 
     const isValid = verifyVnpResponse(queryParams);
     if (!isValid) {
+      console.warn('❌ Checksum không hợp lệ:', queryParams);
       return res.status(400).json({ error: '❌ Checksum không hợp lệ' });
     }
 
-    console.log('✅ Checksum hợp lệ');
+    console.log('✅ Checksum hợp lệ cho callback');
     return res.status(200).json({ message: '✅ Đã xử lý thành công VNPAY callback' });
   } catch (err) {
     console.error('❌ Lỗi xử lý callback:', err);
     return res.status(500).json({ error: 'Lỗi xử lý callback', detail: err.message });
   }
 }
+
